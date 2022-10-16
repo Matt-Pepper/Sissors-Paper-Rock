8ee6283f4bf10cf15524104a813ef3954bae02fb
Matt-Pepper <matthew.g.pepper@gmail.com>
2022-10-16 12:03:41 +1100

    first commit

---
+++ js/script.js
@@ -0,0 +1,112 @@
+const score = {win: 0, lose: 0, draw: 0}
+
+const randomInt = () => {
+	return Math.floor(Math.random() * 3);
+};
+
+const getOpponentMove = () => {
+	const moves = { scissors: 0, paper: 0, rock: 0 };
+	for (let i = 0; i < 19999; i++) {
+		switch (randomInt()) {
+			case 0:
+				moves.scissors++;
+				break;
+			case 1:
+				moves.paper++;
+				break;
+			case 2:
+				moves.rock++;
+				break;
+		}
+	}
+	return Object.keys(moves).reduce((a, b) => (moves[a] > moves[b] ? a : b));
+};
+
+const buttonEvents = () => {
+	const buttons = document.querySelectorAll(".game__button");
+	for (const button of buttons) {
+		button.addEventListener("click", getMove);
+	}
+};
+
+
+const getMove = (e) => {
+    const opponent = getOpponentMove();
+    const move = e.target.value;
+	const result = { win: "win", lose: "lose", draw: "draw" };
+
+
+    changeOpponent(opponent)
+	if (move === opponent) {
+		gameResult(result.draw, e);
+        score.draw++
+        updateScore(e);
+		return;
+	}
+	if (
+		(move === "paper" && opponent === "rock") ||
+		(move === "rock" && opponent === "scissors") ||
+		(move === "scissors" && opponent === "paper")
+	) {
+		gameResult(result.win, e);
+        score.win++
+        updateScore(e);
+		return;
+	}
+	gameResult(result.lose, e);
+    score.lose++
+    updateScore(e);
+	return;
+};
+
+const gameResult = (result, e) => {
+	const opponent = document.querySelector(".game__opponent");
+
+	const win = "game__opponent--win";
+	const lose = "game__opponent--lose";
+	switch (result) {
+		case "win":
+			opponent.classList.remove(lose);
+			opponent.classList.add(win);
+            
+			break;
+		case "lose":
+			opponent.classList.remove(win);
+			opponent.classList.add(lose);
+            
+			break;
+		case "draw":
+			opponent.classList.remove(win);
+			opponent.classList.remove(lose);
+            
+			break;
+	}
+};
+const changeOpponent = (move) => {
+	const moveHolder = document.querySelector(".game__opponent-move");
+
+	moveHolder.classList.remove("fa-hands");
+	moveHolder.classList.remove("fa-hand-peace");
+	moveHolder.classList.remove("fa-hand");
+	moveHolder.classList.remove("fa-hand-back-fist");
+	switch (move) {
+		case "paper":
+			moveHolder.classList.add("fa-hand");
+			break;
+		case "rock":
+			moveHolder.classList.add("fa-hand-back-fist");
+			break;
+		case "scissors":
+			moveHolder.classList.add("fa-hand-peace");
+			break;
+	}
+};
+const updateScore = (e) => {
+    const total = score.draw + score.lose + score.win;
+    const ratio = Math.round((score.win + (score.draw / 10) / total) * 100) / 100;
+    const scoreHolder = e.target.closest(".game").querySelector(".game__stats");
+
+    scoreHolder.innerHTML = `Wins: ${score.win} Losses: ${score.lose} Draws: ${score.draw} <br/> Total: ${total} Ratio: ${ratio}`
+}
+
+buttonEvents();
